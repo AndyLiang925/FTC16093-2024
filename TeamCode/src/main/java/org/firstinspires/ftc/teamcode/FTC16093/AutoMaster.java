8ebcb40bfd6e786551fb17c3405c3769ed6b3d2f
Allen Yang <allenyang00716@gmail.com>
2024-03-07 19:37:17 +0800

    yes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC16093/AutoMaster.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC16093/AutoMaster.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class AutoMaster extends LinearOpMode {
 
 
-    private ElapsedTime runtime;//cao
+    private ElapsedTime runtime;
     public static final int PROXIMAL = 1;
     public static final int DISTAL = -1;
     public static boolean DEBUG = true;
